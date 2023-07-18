1397023b064c30b353f6ab15c00a52e183d35303
Larsenv <larsen@weborican.com>
2023-07-18 18:44:36 -0500

    Many improvements

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
-# TiVoRPC
-Discord rich presence for TiVo made in Python3. Tested with a Virgin Media V6 TiVo Box.
+# TiVoRPCPlus
+Discord rich presence for TiVo made in Python3.
 
 ## Installation
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install requirements.
-
-```bash
-pip install -r requirements.txt
-```
+You can grab a ready-to-use 
 
 ## Configuration
 Edit config.json as follows:
@@ -14,18 +10,12 @@ Edit config.json as follows:
 | Key | Value |
 | ------ | ------ |
 | tivo_ip | Your TiVo Box IP |
-| client_id | Your RPC Client ID (Check Important Notes) |
-| large_image | RPC Art Asset Name To Use (Check Important Notes) |
-
-## Usage
-
-```bash
-python TiVoRPC.py
-```
+| tvguide_url | Zap2it Program Guide Link (see Important Notes) |
 
 ## Important Notes
-You __NEED__ to create an application [here](https://discord.com/developers/applications/) and grab your client ID, then create an "Art Asset" that will be displayed next to your status.
+
+To get your `tvguide_url` you will need to go on [Zap2it](https://tvschedule.zap2it.com/) website then correctly select your provider. In Developer Mode, go on Network then you can find a URL with `grid` in its name. Copy that to the config.json
 
 You __NEED__ to enable Network Remote Control in your TiVo settings. Google how to do this for your specific TiVo box.
 
-Thanks to [David Newcomb](https://github.com/davidnewcomb) for his [JSON Virgin Media Channel Listings](http://www.bigsoft.co.uk/tools/virgin-media-channel-finder)
+Thanks to dwilliamsuk for the original TiVoRPC app.
--- TiVoRPCPlus.py
+++ TiVoRPCPlus.py
@@ -1,4 +1,5 @@
 from pypresence import Presence
+import callsigns
 import time
 import socket
 import json
@@ -16,7 +17,7 @@ TCP_PORT = 31339
 BUFFER_SIZE = 30
 
 ## RPC Config
-client_id = configjson["client_id"]
+client_id = "993635110333198449"
 RPC = Presence(client_id)
 RPC.connect()
 
@@ -45,34 +46,88 @@ tivo_images = {
     "846": "roamio",
     "848": "roamiopro",
     "849": "bolt",
+    "A92": "mini",
+    "A95": "minivox",
     "D6E": "edge",
     "D6F": "edge",
 }
 
+tivo_names = {
+    "110": "TiVo Series 2 Sony SVR-3000",
+    "130": "TiVo Series 2 AT&T 130",
+    "140": "TiVo Series 2 140",
+    "230": "TiVo Series 2 AT&T 230",
+    "264": "TiVo Series 2 Toshiba",
+    "275": "TiVo Series 2 Pioneer DVD Burner",
+    "540": "TiVo Series 2 540",
+    "565": "TiVo Series 2 Toshiba DVD Burner",
+    "590": "TiVo Series 2 Humax",
+    "595": "TiVo Series 2 Humax DVD Burner",
+    "648": "TiVo Series 3 OLED",
+    "649": "TiVo Series 2 Dual Tuner",
+    "652": "TiVo Series 3 HD",
+    "658": "TiVo Series 3 XL",
+    "746": "TiVo Premiere",
+    "748": "TiVo Premiere XL",
+    "750": "TiVo Premiere 4",
+    "758": "TiVo Premiere XL4",
+    "840": "TiVo Roamio Pro",
+    "846": "TiVo Roamio",
+    "848": "TiVo Roamio Plus",
+    "849": "TiVo Bolt",
+    "A92": "TiVo Mini",
+    "A95": "TiVo Mini VOX",
+    "D6E": "TiVo Edge Cable",
+    "D6F": "TiVo Edge Antenna",
+}
+
 ## Get TiVo Image
-def getTiVoImage():
+def getTiVoModel():
     try:
         response = requests.head("http://" + configjson["tivo_ip"] + "/")
-        image = tivo_images[response.headers.get("Server").split(":")[2]]
-        return image
+        return response.headers.get("Server").split(":")[2]
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
         return None
 
 
+## Get TiVo Image
+def getTiVoImage(model):
+    image = tivo_images[model]
+    return image
+
+
+## Get TiVo Name
+def getTiVoName(model):
+    image = tivo_names[model]
+    return image
+
+
 ## Get Current Channel
 def getChan():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((TCP_IP, TCP_PORT))
+    try:
+        s.connect((TCP_IP, TCP_PORT))
+    except ConnectionRefusedError:
+        print(
+            "Connection Refused. This probably means your TiVo doesn't have network remote control enabled."
+        )
+        sys.exit(1)
     data = s.recv(BUFFER_SIZE)
     s.close()
     ccn = "".join(filter(str.isdigit, str(data)))
-    if len(ccn) == 8:
-        ccn2 = str(int(ccn[-4:]))
-        ccn = str(int(ccn[:-4]))
-        ccn += "-" + ccn2
-    else:
-        ccn = str(int(ccn))
+    try:
+        if len(ccn) == 8:
+            ccn2 = str(int(ccn[-4:]))
+            ccn = str(int(ccn[:-4]))
+            ccn += "-" + ccn2
+        else:
+            ccn = str(int(ccn))
+    except ValueError:
+        print(
+            "Couldn't find the channel you're on. Try going into TiVo Central and exit it."
+        )
+        sys.exit(1)
     if b"RECORDING" in data:
         ccn += " 🔴"
     return ccn
@@ -98,8 +153,11 @@ def getName(guide_data, num):
 
     if guide_data:
         for channel in guide_data:
-            if channel["channelNo"] == num.replace("-", "."):
-                return channel["callSign"]
+            if channel["channelNo"] == num:
+                if channel["callSign"] in callsigns.callsigns:
+                    return callsigns.callsigns[channel["callSign"]]
+                else:
+                    return channel["callSign"]
 
     return False
 
@@ -110,8 +168,14 @@ def getFullName(guide_data, num):
 
     if guide_data:
         for channel in guide_data:
-            if channel["channelNo"] == num.replace("-", "."):
-                return channel["affiliateName"]
+            if channel["channelNo"] == num:
+                if channel["callSign"] not in callsigns.callsigns:
+                    if channel["affiliateName"] != "":
+                        return " - " + channel["affiliateName"]
+                    else:
+                        return ""
+                else:
+                    return ""
 
     return False
 
@@ -122,8 +186,20 @@ def getShowName(guide_data, num):
 
     if guide_data:
         for channel in guide_data:
-            if channel["channelNo"] == num.replace("-", "."):
+            if channel["channelNo"] == num:
                 return channel["events"][0]["program"]["title"]
+
+    return False
+
+
+## Get Show Name from Number
+def getEpisodeTitle(guide_data, num):
+    num = str(num.replace("-", ".").split(" ")[0])
+
+    if guide_data:
+        for channel in guide_data:
+            if channel["channelNo"] == num:
+                return channel["events"][0]["program"]["episodeTitle"]
 
     return False
 
@@ -132,41 +208,64 @@ global i
 
 i = 1
 
+current_time = time.time()
+
 ## RPC Updater
 def updateRPC():
     global pcn
     ccn = getChan()
+    activity = ""
     guide_data = getGuideData()
     name = getName(guide_data, ccn)
+    if name is False:
+        name = ""
     full_name = getFullName(guide_data, ccn)
-    image = getTiVoImage()
-    if name != full_name and name is not None:
-        name += " - " + full_name
-    if name is None:
-        name = full_name
+    if full_name is False:
+        full_name = ""
+    model = getTiVoModel()
+    image = getTiVoImage(model)
+    tivo_name = getTiVoName(model)
     show_name = getShowName(guide_data, ccn)
+    episode_title = getEpisodeTitle(guide_data, ccn)
     if show_name is not False:
-        ccn += " - " + show_name
-    if "🔴" in ccn:
-        ccn = ccn.replace(" 🔴", "") + " 🔴"
-    if ccn == pcn or i % 6 == 0:
+        activity += show_name
+    if episode_title:
+        if len(activity + episode_title) <= 62:
+            activity += " - " + episode_title
+    name += " - Channel " + ccn
+    if name != full_name and name is not None:
+        name += full_name
+    if "🔴" in name:
+        name = name.replace(" 🔴", "")
+        activity += " - " + "🔴 REC"
+    if activity == pcn or i % 6 == 0:
         return "No need to update, sleeping for 5 seconds"
     elif ccn.split("-")[0].split(" ")[0].isnumeric() == False:
         return "Error while grabbing current channel, ensure that TiVo IP is correct or that multiple instances aren't running"
     else:
         pcn = ccn
         if name == False:
+            current_time = time.time()
+
             return RPC.update(
-                state="Channel " + ccn,
+                state=activity,
                 large_image=image,
-                large_text="Channel " + ccn,
+                large_text=tivo_name,
+                small_image="tivologo",
+                small_text="TiVo Logo",
+                start=current_time,
             )
         else:
+            current_time = time.time()
+
             return RPC.update(
-                state="Channel " + ccn,
+                state=activity,
                 details=name,
                 large_image=image,
-                large_text="Channel " + ccn,
+                large_text=tivo_name,
+                small_image="tivologo",
+                small_text="TiVo Logo",
+                start=current_time,
             )
 
 
